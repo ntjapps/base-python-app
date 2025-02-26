33278721d8d7e9a542480c5189a6ee1cbbedf256
Nathanael Yusuf Tj <26967808+nathanaelytj@users.noreply.github.com>
2025-02-26 09:34:33 +0000

    Consolidated 26 Feb 2025 - Laravel 12 Update

--- worker.py
+++ worker.py
@@ -1,6 +1,25 @@
-from celery import Celery
+from celery import Celery, signals
+import os
+import sentry_sdk
 
 celery = Celery(__name__)
 celery.config_from_object("worker-config")
 
 celery.autodiscover_tasks(["jobs.test_job", "jobs.logger_job"])
+
+webhook_endpoint = os.getenv("APP_URL")
+celry_dsn = os.getenv("CELERY_DSN")
+
+
+@signals.celeryd_init.connect
+def init_sentry(**_kwargs):
+    sentry_sdk.init(
+        dsn=celry_dsn,
+        # Set traces_sample_rate to 1.0 to capture 100%
+        # of transactions for tracing.
+        traces_sample_rate=1.0,
+        # Set profiles_sample_rate to 1.0 to profile 100%
+        # of sampled transactions.
+        # We recommend adjusting this value in production.
+        profiles_sample_rate=1.0,
+    )
